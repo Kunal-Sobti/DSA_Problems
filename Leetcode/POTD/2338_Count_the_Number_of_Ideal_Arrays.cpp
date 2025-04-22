dae108e45f8f691dadc7bca56e7da1ba1965cefb
Kunal <kunalsobtiks@gmail.com>
2025-04-22 23:29:42 +0530

    POTD 22nd APRIL 2025

---
+++ Leetcode/POTD/2338_Count_the_Number_of_Ideal_Arrays.cpp
@@ -0,0 +1,17 @@
+typedef long long ll;
+#define vi vector<int>
+#define fi for(int i=0;i<n;i++)
+#include<bits/stdc++.h>
+using namespace std;
+
+//The most powerful version of you is the most disciplined version of you
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int t=0;
+    cin>>t;
+    while(t--){
+        
+    }
+    return 0;
+}
